9ac8a398989c53872b7a42f1da3028f89ebac7b7
Vinnie <vincentathorne2005@gmail.com>
2024-07-15 11:42:27 -0700

    scatter plot chart

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -1,28 +1,29 @@
-import { useState, useEffect } from "react";
 import {
   ScatterChart,
   Scatter,
+  CartesianGrid,
   XAxis,
   YAxis,
-  CartesianGrid,
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
+import { useState, useEffect } from "react";
 
 const CityEventsChart = ({ allLocations, events }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     setData(getData());
-  }, [`${data}`]);
+    //when user updates city or number of events we want useEffect to re-execute. this is why we use stringified value inside dependency array
+  }, [`${events}`]);
 
   const getData = () => {
     const data = allLocations.map((location) => {
       const count = events.filter(
         (event) => event.location === location
       ).length;
-      const city = location.split(", ")[0];
-      return { city, count };
+      const city = location.split(/, | - /)[0];
+      return { city, count }; //shorthand for {city: city, count: count}
     });
     return data;
   };
@@ -33,12 +34,19 @@ const CityEventsChart = ({ allLocations, events }) => {
         margin={{
           top: 20,
           right: 20,
-          bottom: 20,
-          left: 20,
+          bottom: 60,
+          left: -30,
         }}
       >
-        <CartesianGrid />
-        <XAxis type="category" dataKey="city" name="City" />
+        <CartesianGrid fill="#a7e0d9" fillOpacity={0.4} />
+        <XAxis
+          type="category"
+          dataKey="city"
+          name="City"
+          angle={60}
+          interval={0}
+          tick={{ dx: 20, dy: 40, fontSize: 14 }}
+        />
         <YAxis
           type="number"
           dataKey="count"
@@ -46,7 +54,7 @@ const CityEventsChart = ({ allLocations, events }) => {
           allowDecimals={false}
         />
         <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-        <Scatter name="A school" data={data} fill="#8884d8" />
+        <Scatter name="Events" data={data} fill="#ff0000" />
       </ScatterChart>
     </ResponsiveContainer>
   );
